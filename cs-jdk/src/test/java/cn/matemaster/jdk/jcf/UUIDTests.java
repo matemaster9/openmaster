f750422f7517f408c761258a4479cea347ca2dd9
matemaster <matemaster@163.com>
2022-02-28 10:29:19 +0800

    test: UUID

---
+++ cs-jdk/src/test/java/cn/matemaster/jdk/jcf/UUIDTests.java
@@ -0,0 +1,18 @@
+package cn.matemaster.jdk.jcf;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+/**
+ * @Description:
+ * @Author: MateMaster
+ * @Date: 2022/2/28 10:26
+ */
+public class UUIDTests {
+
+    @Test
+    void autoTest() {
+        System.out.println(UUID.randomUUID());
+    }
+}
